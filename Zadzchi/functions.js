8e383a469775414020d7f66a3ec31ead9ffffe40
Maxim Rudak <kaes.cooll@mail.ru>
2025-02-19 17:46:42 +0300

    Commit

---
+++ Zadzchi/functions.js
@@ -0,0 +1,48 @@
+// functions.js
+function myMap(array, callback) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        result.push(callback(array[i], i, array));
+    }
+    return result;
+}
+
+function myFilter(array, callback) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i], i, array)) {
+            result.push(array[i]);
+        }
+    }
+    return result;
+}
+
+function myFind(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i], i, array)) {
+            return array[i];
+        }
+    }
+    return undefined;
+}
+
+function mySome(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i], i, array)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function myEvery(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        if (!callback(array[i], i, array)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Экспортируем функции
+module.exports = { myMap, myFilter, myFind, mySome, myEvery };
